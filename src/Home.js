643f00a3df50a9e2924cd70c5bddbeab0e2b0422
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-06-07 10:15:43 +0600

    added:contact,about

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div style={{textAlign:'center'}}>
+        <h4 style={{background:'red',borderRadius:'20px',color:'white',padding:'10px',textAlign:'center'}}>Home</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia aut rem suscipit ipsam voluptatibus, tempora eaque sunt corrupti veniam beatae.</p>
+    </div>
+  )
+}
+
+export default Home

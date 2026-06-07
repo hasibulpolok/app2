34fd1183729a1b2a0073f95c27d11f051a1225a1
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-06-07 10:32:22 +0600

    router added

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,9 @@
+
 import React from 'react'
 
 function Contact() {
   return (
-    <div style={{textAlign:'center'}}>
+      <div style={{textAlign:'center'}}>
         <h4 style={{background:'red',borderRadius:'20px',color:'white',padding:'10px',textAlign:'center'}}>Contact</h4>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia aut rem suscipit ipsam voluptatibus, tempora eaque sunt corrupti veniam beatae.</p>
     </div>

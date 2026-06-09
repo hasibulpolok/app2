1ca56998d4b9a529f062b16dcfd027fd6bbfcdcc
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-06-09 11:14:58 +0600

    ternary

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="mt-5 p-4 bg-dark text-white text-center">
-      <p>Footer</p>
+      <p>Developed By Hasibul Polok.</p>
     </div>
   );
 }
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default function Events() {
   const shoot = (a) => {
     alert(a);
   };
+  const Today = "Tuesday";
 
   return (
-    <div className='container-fluid'>
-      <div className='mb-2'>Events</div>
-
-      <button onClick={() => shoot("Goal!")}>
-        Click Me
-      </button>
+    <div className="container-fluid">
+      <div className="mb-2">Events</div>
+      <button onClick={() => shoot("Goal!")}>Click Me</button> 
+      <br />
+      {(Today == "Tuesday" ? "Office is open" : "Office is closed")}; 
+      <br />
+      {Today == "Friday" && "Office is closed"}
     </div>
   );
 }
